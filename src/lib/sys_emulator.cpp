66a45e2c70fc24f4b3a64db8d0d2280040770518
UserDeploy <thisisnotpremium@gmail.com>
2025-03-31 21:42:51 +0700

    add CP15

--- src/lib/sys_emulator.cpp
+++ src/lib/sys_emulator.cpp
@@ -9,6 +9,7 @@
 
 #include "dynarmic/interface/A32/a32.h"
 #include "dynarmic/interface/A32/config.h"
+#include "dynarmic/interface/A32/coprocessor.h"
 #include "dynarmic/interface/exclusive_monitor.h"
 
 #include "mcl/assert.hpp"
@@ -287,10 +288,64 @@ namespace Dynemu {
         return cpu->Regs()[0];
     }
 
+    class CP15 final : public Dynarmic::A32::Coprocessor {
+        using Callback = Dynarmic::A32::Coprocessor::Callback;
+        using CoprocReg = Dynarmic::A32::CoprocReg;
+        using CallbackOrAccessOneWord = Dynarmic::A32::Coprocessor::CallbackOrAccessOneWord;
+        using CallbackOrAccessTwoWords = Dynarmic::A32::Coprocessor::CallbackOrAccessTwoWords;
+    
+        u32 threadStoragePointer; // Pointer to thread-local storage
+
+        std::optional<Callback> CompileInternalOperation(bool two, unsigned opc1, CoprocReg CRd,
+            CoprocReg CRn, CoprocReg CRm,
+            unsigned opc2) override {
+            return std::nullopt;
+        }
+    
+        CallbackOrAccessOneWord CompileSendOneWord(bool two, unsigned opc1, CoprocReg CRn,
+            CoprocReg CRm, unsigned opc2) override {
+            return CallbackOrAccessOneWord{};
+        }
+    
+        CallbackOrAccessTwoWords CompileSendTwoWords(bool two, unsigned opc, CoprocReg CRm) override {
+            return CallbackOrAccessTwoWords{};
+        }
+    
+        CallbackOrAccessOneWord CompileGetOneWord(bool two, unsigned opc1, CoprocReg CRn,
+            CoprocReg CRm, unsigned opc2) override {
+            // Stores a pointer to thread-local storage, accessed via mrc p15, 0, rd, c13, c0, 3
+            if (!two && CRn == CoprocReg::C13 && opc1 == 0 && CRm == CoprocReg::C0 && opc2 == 3) {
+                return &threadStoragePointer;
+            }
+    
+            return CallbackOrAccessOneWord{};
+        }
+    
+        CallbackOrAccessTwoWords CompileGetTwoWords(bool two, unsigned opc, CoprocReg CRm) override {
+            return CallbackOrAccessTwoWords{};
+        }
+    
+        std::optional<Callback> CompileLoadWords(bool two, bool long_transfer, CoprocReg CRd,
+            std::optional<std::uint8_t> option) override {
+            return std::nullopt;
+        }
+    
+        std::optional<Callback> CompileStoreWords(bool two, bool long_transfer, CoprocReg CRd,
+            std::optional<std::uint8_t> option) override {
+            return std::nullopt;
+        }
+    
+    public:
+        void setTLSBase(u32 value) {
+            threadStoragePointer = value;
+        }
+    };
+
     Emulator::Emulator() {
         Dynarmic::A32::UserConfig user_config;
 
         user_config.callbacks = &env;
+        user_config.coprocessors[15] = std::make_shared<CP15>();
         user_config.enable_cycle_counting = false;
         user_config.arch_version = Dynarmic::A32::ArchVersion::v7;
 
